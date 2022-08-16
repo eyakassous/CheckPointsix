117f1ba3031033922cf0f820d5d91613a8fccddd
eyakassous <eyakassous55@gmail.com>
2022-08-16 14:24:18 +0100

    learn state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = { fullName: 'eya kassous', bio: '###', imgSrc: '/profil_img.png', profession: 'etudiante', show: false, count: 0, intervalId: 0 };
+
+  componentDidMount() {
+    const intervalId = setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  handleshow = () => {
+    if (this.state.show) {
+      this.setState({ show: false })
+    }
+    else {
+      this.setState({ show: true })
+    }
+    console.log(this.state.show);
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center', fontFamily: 'georgia', fontWeight: 'bold' }}>
+        {this.state.show && <div ><h3>{this.state.fullName}</h3>
+          <h3>{this.state.bio}</h3>
+          <h3>{this.state.imgSrc}</h3>
+          <h3>{this.state.profession}</h3></div >}
+        <button onClick={this.handleshow}>show</button>
+        <h1>crono: {this.state.count} seconds</h1>
+      </div>
+    );
+  }
 }
-
 export default App;
